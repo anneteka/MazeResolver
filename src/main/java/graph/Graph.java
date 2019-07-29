24b6b9cd8f787d9f0ce7b6298b1fb3f6d6b58806
Anna <a.karlysheva@ukma.edu.ua>
2019-07-29 22:01:34 +0300

    basic graph structure

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,5 +1,18 @@
 package graph;
 
+import java.util.ArrayList;
+
 public class Graph {
     //contains information about the maze as graph
+
+    private int vertices; //amount of vertices
+    private int edges; //amount of edges
+    private ArrayList<Integer>[] adj; //list of all adjacent vertices
+
+    //data string contains all information about the graph
+    //and has the structure as below
+    //vertices_amount edges_amount start_vertex finish_vertex vertex-vertex ... pairs of linked vertices
+    public Graph(String data){
+
+    }
 }
