762e8247f69d4974712ae773c2a909abde8f87dd
Anna <a.karlysheva@ukma.edu.ua>
2019-07-30 18:15:38 +0300

    bfs base

--- src/main/java/graph/BreadthFirstSearch.java
+++ src/main/java/graph/BreadthFirstSearch.java
@@ -3,9 +3,15 @@ package graph;
 public class BreadthFirstSearch {
     //calculates the shortest possible path
 
+    private static final int INFINITY = -1;
+    private boolean[] marked;  //is there an s-v path
+    private int[] edgeTo;   //previous edge on shortest s-v path
+    private int[] distTo;   //number of edges shortest s-v path
 
     //int source - start vertex in the maze
     public BreadthFirstSearch(Graph g, int source){
-
+        marked = new boolean[g.getVertices()];
+        distTo = new int[g.getVertices()];
+        edgeTo = new int[g.getVertices()];
     }
 }
