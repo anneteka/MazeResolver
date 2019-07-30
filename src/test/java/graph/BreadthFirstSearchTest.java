b04dc9dda53f36abd7cc0788377d0b3553b23b29
Anna <a.karlysheva@ukma.edu.ua>
2019-07-30 20:05:23 +0300

    bfs test base

---
+++ src/test/java/graph/BreadthFirstSearchTest.java
@@ -0,0 +1,16 @@
+package graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BreadthFirstSearchTest {
+
+    @Test
+    public void pathTo() {
+    }
+
+    @Test
+    public void hasPathTo() {
+    }
+}
