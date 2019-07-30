2957ccbd1b0ef369002bf1ef7fee04604ec8dccb
Anna <a.karlysheva@ukma.edu.ua>
2019-07-30 17:49:24 +0300

    Graph toString();

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -17,6 +17,9 @@ public class Graph {
         String[] graph = data.split(" ");
         vertices = Integer.parseInt(graph[0]);
         adj = new ArrayList[vertices];
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
         startVertex = Integer.parseInt(graph[graph.length - 2]);
         finishVertex = Integer.parseInt(graph[graph.length - 1]);
         edges = graph.length - 3;
@@ -46,5 +49,24 @@ public class Graph {
 
     public ArrayList<Integer>[] getAdj() {
         return adj;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(vertices);
+        sb.append(' ');
+        for (int i = 0; i < adj.length; i++) {
+            for (int vertex : adj[i]) {
+                sb.append(i);
+                sb.append('-');
+                sb.append(vertices);
+                sb.append(' ');
+            }
+        }
+        sb.append(startVertex);
+        sb.append(' ');
+        sb.append(finishVertex);
+        return sb.toString();
     }
 }
