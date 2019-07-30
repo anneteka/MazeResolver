a7d1d221f094359d2744ff6dcf9ab06f654e8b93
Anna <a.karlysheva@ukma.edu.ua>
2019-07-30 18:09:18 +0300

    Dijkstra -> breadth first search

---
+++ src/main/java/graph/BreadthFirstSearch.java
@@ -0,0 +1,11 @@
+package graph;
+
+public class BreadthFirstSearch {
+    //calculates the shortest possible path
+
+
+    //int source - start vertex in the maze
+    public BreadthFirstSearch(Graph g, int source){
+
+    }
+}
