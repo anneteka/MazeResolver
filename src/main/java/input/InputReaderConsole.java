ab5ff65849a80d2943845dae38968e1ab747516b
Anna <a.karlysheva@ukma.edu.ua>
2019-07-30 12:39:16 +0300

    read from console

--- src/main/java/input/InputReaderConsole.java
+++ src/main/java/input/InputReaderConsole.java
@@ -1,11 +1,24 @@
 package input;
 
+import java.util.Scanner;
+
 public class InputReaderConsole extends AbstractInputReader {
+    private Scanner scanner;
 
-    public InputReaderConsole(){
-
+    public InputReaderConsole() {
+        scanner = new Scanner(System.in);
     }
+
     String readMaze() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        System.out.print("Enter the number of lines in the maze: ");
+        int lines = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the maze: ");
+        for (int i = 0; i < lines; i++) {
+            sb.append(scanner.nextLine());
+            sb.append("|");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
