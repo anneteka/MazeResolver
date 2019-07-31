b3a12f6d2d21e5db1114ee2a0c09e9d2975c2f48
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 12:35:08 +0300

    case for no path

--- src/main/java/processors/PathToString.java
+++ src/main/java/processors/PathToString.java
@@ -3,22 +3,28 @@ package processors;
 import java.util.ArrayList;
 
 public class PathToString {
-    static String pathToString(ArrayList<Integer> path){
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < path.size()-1; i++) {
-            int diff = path.get(i)-path.get(i+1);
-            if (diff==1)
-                sb.append("r, ");
-            else if (diff==-1)
-                sb.append("l, ");
-            else if (diff<-1)
-                sb.append("u, ");
-            else
-                sb.append("d, ");
+    static String pathToString(ArrayList<Integer> path) {
 
+        StringBuilder sb = new StringBuilder();
+        if (path == null) {
+            sb.append("no path found");
+        } else {
+            for (int i = 0; i < path.size() - 1; i++) {
+                int diff = path.get(i) - path.get(i + 1);
+                if (diff == 1)
+                    sb.append("r, ");
+                else if (diff == -1)
+                    sb.append("l, ");
+                else if (diff < -1)
+                    sb.append("u, ");
+                else
+                    sb.append("d, ");
+
+            }
+            sb.deleteCharAt(sb.length() - 2);
+            sb.deleteCharAt(sb.length() - 1);
         }
-        sb.deleteCharAt(sb.length()-2);
-        sb.deleteCharAt(sb.length()-1);
         return sb.toString();
+
     }
 }
