8fcb5331233cee6746bb69f664cc8e4108631be1
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 21:02:25 +0300

    String data -> String[]

--- src/main/java/processors/InputProcessor.java
+++ src/main/java/processors/InputProcessor.java
@@ -4,15 +4,14 @@ import graph.Graph;
 
 public class InputProcessor {
     //processes data from input readers and forms a graph
-    public static Graph createGraph(String data) {
+    public static Graph createGraph(String[] data) {
         return new Graph(process(data));
     }
 
     //returns string that contains all information about the graph
     //and has the structure as below
     //vertices_amount start_vertex finish_vertex vertex-vertex ... pairs of linked vertices
-    public static String process(String data) {
-        String[] maze = data.split("\\|");
+    public static String process(String[] maze) {
         StringBuilder graph = new StringBuilder();
         graph.append(maze[0].length() * maze.length);
         graph.append(' ');
