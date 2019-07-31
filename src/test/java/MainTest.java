67a6bf5f32814d9c590f429ac7989ddee97d5137
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 19:34:00 +0300

    input reader from file test, overall test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,5 @@
 
+import input.InputReaderFile;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +9,17 @@ public class MainTest {
     @Test
     public void overallTest(){
        assertEquals("d, d", PathFinder.findPathFile("testFiles/maze2.txt"));
+        assertEquals("no path found", PathFinder.findPathFile("testFiles/maze4.txt"));
     }
 
-}
+    @Test
+    public void graphTest(){
+
+    }
+
+    @Test
+    public void inputReaderFileTest(){
+        InputReaderFile reader = new InputReaderFile("testFiles/smallMaze.txt");
+        assertEquals("S.|.X", reader.readMaze());
+    }}
+
