67ec1a490d95a944d71c7a6cf58eb3f5090ae815
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 12:30:59 +0300

    path to string done

--- src/main/java/processors/PathToString.java
+++ src/main/java/processors/PathToString.java
@@ -1,7 +1,24 @@
 package processors;
 
+import java.util.ArrayList;
+
 public class PathToString {
-    static String pathToString(){
-        return null;
+    static String pathToString(ArrayList<Integer> path){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < path.size()-1; i++) {
+            int diff = path.get(i)-path.get(i+1);
+            if (diff==1)
+                sb.append("r, ");
+            else if (diff==-1)
+                sb.append("l, ");
+            else if (diff<-1)
+                sb.append("u, ");
+            else
+                sb.append("d, ");
+
+        }
+        sb.deleteCharAt(sb.length()-2);
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
     }
 }
