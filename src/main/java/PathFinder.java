cc1d5cce9172d1c3470e3a27b329d76e979a6816
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 18:11:25 +0300

    find path method

--- src/main/java/PathFinder.java
+++ src/main/java/PathFinder.java
@@ -1,14 +1,25 @@
 import graph.BreadthFirstSearch;
 import graph.Graph;
 import input.AbstractInputReader;
+import input.InputReaderConsole;
+import input.InputReaderFile;
 import processors.InputProcessor;
+import processors.PathToString;
 
 public class PathFinder {
-    private static String findPathConsole(){
-        return null;
+    public static String findPathConsole(){
+        return findPath(new InputReaderConsole());
     }
-    private static String findPathFile(){
-        return null;
+    public static String findPathFile(String file){
+        return findPath(new InputReaderFile(file));
     }
 
+    private static String findPath(AbstractInputReader reader){
+        String mazeData = reader.readMaze();
+        String processedMazeData = InputProcessor.process(mazeData);
+        Graph maze = new Graph(processedMazeData);
+        BreadthFirstSearch bfs = new BreadthFirstSearch(maze);
+        String path = PathToString.pathToString(maze, bfs.pathTo(maze.getFinishVertex()));
+        return path;
+    }
 }
