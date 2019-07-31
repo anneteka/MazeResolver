98822a3f53aff31a08a4c9078f62bbd06d789cfe
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 18:12:21 +0300

    main test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,13 @@
+
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
     public void overallTest(){
-
+       assertEquals("d, d", PathFinder.findPathFile("testFiles/maze2.txt"));
     }
 
 }
