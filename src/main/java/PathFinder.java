5536d2ad04c30b3c3a62ee03feb3da54041a46c8
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 16:38:36 +0300

    path finder baze

---
+++ src/main/java/PathFinder.java
@@ -0,0 +1,14 @@
+import graph.BreadthFirstSearch;
+import graph.Graph;
+import input.AbstractInputReader;
+import processors.InputProcessor;
+
+public class PathFinder {
+    private static String findPathConsole(){
+        return null;
+    }
+    private static String findPathFile(){
+        return null;
+    }
+
+}
