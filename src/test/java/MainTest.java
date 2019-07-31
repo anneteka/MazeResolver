47db638e257ceeb7b5c2f463cebd3d0b05953cda
Anna <a.karlysheva@ukma.edu.ua>
2019-07-31 21:31:40 +0300

    updated tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,24 +2,31 @@
 import input.InputReaderFile;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
-    public void overallTest(){
-       assertEquals("d, d", PathFinder.findPathFile("testFiles/maze2.txt"));
-        assertEquals("no path found", PathFinder.findPathFile("testFiles/maze4.txt"));
+    public void overallTest() throws FileNotFoundException {
+        PathFinder pathFinder = new PathFinder();
+        assertEquals("d, d", pathFinder.findPathFile("testFiles/maze2.txt"));
+        assertEquals("no path found", pathFinder.findPathFile("testFiles/maze4.txt"));
     }
 
     @Test
-    public void graphTest(){
+    public void graphTest() {
 
     }
 
     @Test
-    public void inputReaderFileTest(){
-        InputReaderFile reader = new InputReaderFile("testFiles/smallMaze.txt");
-        assertEquals("S.|.X", reader.readMaze());
-    }}
+    public void inputReaderFileTest() throws FileNotFoundException {
+        InputReaderFile reader = new InputReaderFile();
+        String[] maze = {};
+        assertEquals(maze, reader.readMaze(new FileInputStream(new File("testFiles/smallMaze.txt"))));
+    }
+}
 
